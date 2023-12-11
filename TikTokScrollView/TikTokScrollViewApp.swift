87ab1b56aef032e8935a54e812abbd35cffd5542
smbrmoyo <brianmoyou@gmail.com>
2023-12-11 21:22:17 +0100

    Initial Commit

---
+++ TikTokScrollView/TikTokScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokScrollViewApp.swift
+//  TikTokScrollView
+//
+//  Created by Brian Moyou on 11.12.23.
+//
+
+import SwiftUI
+
+@main
+struct TikTokScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
